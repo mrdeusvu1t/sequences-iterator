650ba2cb7dfcab46862aaecc0e29e469b89d78d0
Yury <mr.deusvult@gmail.com>
2021-04-07 21:30:59 +0300

    up

--- SequencesTask/SequenceGenerator.cs
+++ SequencesTask/SequenceGenerator.cs
@@ -18,9 +18,28 @@ namespace SequencesTask
         /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 1.</exception>
         public static IEnumerable<BigInteger> GetFibonacciNumbers(int count)
         {
-            throw new NotImplementedException();
+            if (count < 1)
+            {
+                throw new ArgumentException("Count is less than 1.");
+            }
+
+            return Core(count);
+
+            static IEnumerable<BigInteger> Core(int count)
+            {
+                BigInteger v1 = 0;
+                BigInteger v2 = 1;
+
+                for (int i = 0; i < count; ++i)
+                {
+                    yield return v1;
+                    var tmp = v2;
+                    v2 = v1 + v2;
+                    v1 = tmp;
+                }
+            }
         }
-        
+
         /// <summary>
         /// Generates the sequence of prime numbers.
         /// </summary>
@@ -29,7 +48,42 @@ namespace SequencesTask
         /// <exception cref="ArgumentException">Thrown when <paramref name="count"/> is less than 1.</exception>
         public static IEnumerable<int> GetPrimeNumbers(int count)
         {
-            throw new NotImplementedException();
+            if (count < 1)
+            {
+                throw new ArgumentException("Count is less than 1.");
+            }
+
+            return Core(count);
+
+            static IEnumerable<int> Core(int count)
+            {
+                int n = 2;
+
+                for (int i = 0; i < count;)
+                {
+                    if (IsPrime(n))
+                    {
+                        yield return n;
+                        i++;
+                    }
+                    n++;
+                }
+
+                static bool IsPrime(int n)
+                {
+                    bool isPrime = true;
+
+                    for (int i = 2; i < n; i++)
+                    {
+                        if (n % i == 0)
+                        {
+                            isPrime = false;
+                        }
+                    }
+
+                    return isPrime;
+                }
+            }
         }
 
     }
